467adaf4755d9b55f8c90eada234d3677bd0c4b7
mehtadhiraj <mehtadhiraj21@gmail.com>
2019-10-05 17:51:54 +0530

    first commit

---
+++ app/src/main/java/com/example/dhirajmehta/medicare/HomeFragment.java
@@ -0,0 +1,87 @@
+package com.example.dhirajmehta.medicare;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class HomeFragment  extends Fragment {
+
+    Button submit;
+    View rootview;
+    CheckBox cold, cough, fever, headache, giddyness, stomachache;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.home,container,false);
+        submit = rootview.findViewById(R.id.submitSymptoms);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitSymptoms(v);
+            }
+        });
+
+        return rootview;
+    }
+    void submitSymptoms(View view){
+        // Fetching from database code goes here.
+        ArrayList<String> diseasesList = new ArrayList<String>();
+        String symptoms = "";
+        cold        = rootview.findViewById(R.id.cold);
+        if(cold.isChecked()){
+            diseasesList.add("Cold");
+            symptoms = symptoms + "Cold, ";
+        }
+        cough       = rootview.findViewById(R.id.cough);
+        if(cough.isChecked()){
+            diseasesList.add("Cough");
+            symptoms = symptoms + "Cough, ";
+        }
+        fever       = rootview.findViewById(R.id.fever);
+        if(fever.isChecked()){
+            diseasesList.add("Fever");
+            symptoms = symptoms + "Fever, ";
+        }
+        headache    = rootview.findViewById(R.id.headache);
+        if(headache.isChecked()){
+            diseasesList.add("Headache");
+            symptoms = symptoms + "Headache, ";
+        }
+        giddyness   = rootview.findViewById(R.id.giddyness);
+        if(giddyness.isChecked()){
+            diseasesList.add("Giddyness");
+            symptoms = symptoms + "Giddyness, ";
+        }
+        stomachache = rootview.findViewById(R.id.stomachache);
+        if(stomachache.isChecked()){
+            diseasesList.add("Stomach Ache");
+            symptoms = symptoms + "Stomach Ache. ";
+        }
+        if(symptoms == ""){
+            symptoms = "No item selceted";
+        }else{
+            DashFragment fragment = new DashFragment();
+            Bundle symptomsBundle = new Bundle();
+            symptomsBundle.putString("symptoms", symptoms);
+            fragment.setArguments(symptomsBundle);
+            //Inflate the fragment
+            getFragmentManager().beginTransaction().add(R.id.diseasesView, fragment).commit();
+        }
+        Toast.makeText(getContext(), symptoms, Toast.LENGTH_SHORT).show();
+
+
+    }
+
+}
